b784ff9ae1230b79161761e7f9ae08586e82f1f5
sanjay <fta.sanjay@gmail.com>
2021-10-02 23:57:56 +0530

    Updating Property file with hashmap

--- src/main/java/com/fta/constants/FrameworkConstants.java
+++ src/main/java/com/fta/constants/FrameworkConstants.java
@@ -2,8 +2,13 @@ package com.fta.constants;
 
 public final class FrameworkConstants {
 	private FrameworkConstants() {}
-	private static final String CHROMEDRIVERPATH=System.getProperty("user.dir")+"//src//test//resources//Drivers//chromedriver.exe";
-
+	private static final String RESOURCESPATH=System.getProperty("user.dir")+"/src/test/resources/";
+	private static final String CHROMEDRIVERPATH=RESOURCESPATH+"/Drivers/chromedriver.exe";
+	private static final String CONFIGPROPERTIESPATH=RESOURCESPATH+"config/config.properties";
+	
+	public static String getConfigpropertiespath() {
+		return CONFIGPROPERTIESPATH;
+	}
 	public static String getChromedriverpath() {
 		return CHROMEDRIVERPATH;
 	}
--- src/main/java/com/fta/utils/ReadProperty.java
+++ src/main/java/com/fta/utils/ReadProperty.java
@@ -3,29 +3,44 @@ package com.fta.utils;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
+
+import com.fta.constants.FrameworkConstants;
 
 public final class ReadProperty {
 
 	protected ReadProperty() {	}
 	private static FileInputStream fis;
 	private static Properties property=new Properties();
+	private static Map<String,String> configmap=new HashMap<>();
 	
 	static {
 		try {
-			fis=new FileInputStream(System.getProperty("user.dir")+"/src/test/resources/config/config.properties");
+			fis=new FileInputStream(FrameworkConstants.getConfigpropertiespath());
 			property.load(fis);
+			
+			//adding property value to hashmap because hashtable is slow
+//			for(Object key:property.keySet())
+//			{
+//				configmap.put(String.valueOf(key), String.valueOf(property.get(key)));
+//			}
+			
+			for(Map.Entry<Object,Object> entry:property.entrySet())
+			{
+				configmap.put(String.valueOf(entry.getKey()), String.valueOf(entry.getValue()));
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public static String get(String key) 
+	public static String getvalueusinghashmap(String key) 
 	{
-		if(Objects.isNull(key) || Objects.isNull(property.getProperty(key)))
+		if(Objects.isNull(key) || Objects.isNull(configmap.get(key)))
 		{
 			try {
 				throw new Exception("Property Name "+key+"is not found, please check the config.properties file");
@@ -34,7 +49,22 @@ public final class ReadProperty {
 				e.printStackTrace();
 			}
 		}
-		return property.getProperty(key);		
+		return configmap.get(key);		
 	}
+
+	
+//	public static String get(String key) 
+//	{
+//		if(Objects.isNull(key) || Objects.isNull(property.getProperty(key)))
+//		{
+//			try {
+//				throw new Exception("Property Name "+key+"is not found, please check the config.properties file");
+//			}catch(Exception e)
+//			{
+//				e.printStackTrace();
+//			}
+//		}
+//		return property.getProperty(key);		
+//	}
 
 }
