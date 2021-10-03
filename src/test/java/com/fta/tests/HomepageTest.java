49b96d4532b4562246e26bd8ec90210dfda77f7d
sanjay <fta.sanjay@gmail.com>
2021-10-03 23:54:45 +0530

    updating HomepageTest

--- src/test/java/com/fta/tests/HomepageTest.java
+++ src/test/java/com/fta/tests/HomepageTest.java
@@ -1,5 +1,8 @@
 package com.fta.tests;
 
+import java.util.Objects;
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.fta.driver.DriverManager;
@@ -8,22 +11,20 @@ import com.fta.utils.ReadProperty;
 public final class HomepageTest extends BaseTest{
 	
 	private HomepageTest() {}
+	
+	//Null check
+	//title verification
 	@Test
 	public void test1() 
 	{
 		DriverManager.getDriver().get(ReadProperty.getvalueusinghashmap("url"));
+		String title=DriverManager.getDriver().getTitle();
+		Assert.assertTrue(Objects.nonNull(title));
+		Assert.assertTrue(title.toLowerCase().contains("bewakoof"));
+		
+		Assert.assertEquals(title, "Online Shopping for Men, Women Clothing & Accessories at Bewakoof");
 	}
-//	@Test
-//	public void test2()
-//	{
-//		DriverManager.getDriver().get("https://www.google.com/");
-//	}
-//
-//	@Test
-//	public void test3()
-//	{
-//		DriverManager.getDriver().get("https://www.facebook.com/");
-//	}
+
 
 
 }
