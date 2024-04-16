f69346bfd0f458a2992735f9aaf0c6427fa0b083
sanjaysingh <fta.sanjay@gmail.com>
2024-04-16 12:56:55 +0530

    updating BasePage class by fixing few errors in explicitlyWaitForElementToBeClickable method

--- src/test/java/com/fta/tests/BaseTest.java
+++ src/test/java/com/fta/tests/BaseTest.java
@@ -1,23 +1,39 @@
 package com.fta.tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import com.fta.utils.ExtentReport;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 
 import com.fta.driver.Driver;
 import com.fta.driver.DriverManager;
 import com.fta.utils.ReadProperty;
+import org.testng.annotations.BeforeSuite;
+
+import java.io.IOException;
 
 public class BaseTest {
 	
-	protected BaseTest(){}
+	protected BaseTest(){};
 
+	@BeforeSuite
+	public void beforesuit()
+	{
+		ExtentReport.getextentreport();
+	}
+	@AfterSuite
+	public void aftersuit() throws IOException {
+		ExtentReport.flushreport();
+	}
 	@BeforeMethod
 	public void setup()
 	{
 		Driver.initDriver();
 		DriverManager.getDriver().get(ReadProperty.getvalueusinghashmap("url"));
 	}
-
 	@AfterMethod
 	public void tearDown()
 	{
--- src/test/java/com/fta/tests/LoginpageTest.java
+++ src/test/java/com/fta/tests/LoginpageTest.java
@@ -1,5 +1,7 @@
 package com.fta.tests;
 
+import com.aventstack.extentreports.ExtentTest;
+import com.fta.utils.ExtentReport;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,14 +14,14 @@ public final class LoginpageTest extends BaseTest{
 	@Test
 	public void loginLogoutTest()
 	{
+		ExtentReport.extenttest("Login test");
 		LoginPage loginpage=new LoginPage();
 		String title=loginpage.Login("sanjaysinghjsg@gmail.com", "Evry@2021").logout().getTitle();
+		ExtentReport.test.pass("Login to Bewakoof is successful");
 //		System.out.println(title);
 //		Assert.assertEquals(title, "Online Shopping for Men, Women Clothing & Accessories at Bewakoof");
-		
-		
-		//	Assert.assertEquals(accountname, "Hi, sanjay", "Login has not done completly");
-		
+//		Assert.assertEquals(accountname, "Hi, sanjay", "Login has not done completly");
+
 	}
 
 }
